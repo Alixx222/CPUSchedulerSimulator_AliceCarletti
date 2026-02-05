125acd681749796d40672406c739eca0f616c277
Alixx222 <alicarletti08@gmail.com>
2026-02-05 10:45:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function inputProcess() {
  Il contenuto dei div relativi all'output viene cancellato
 */
 function reset(){
+  p = [];
+at = [];
+bt = [];
+rbt = [];
+pr = [];
+
    let tableEl = document.getElementById("idTable");
    let oldTBodyEl = tableEl.getElementsByTagName('tbody')[0];
    let newTBodyEl = document.createElement('tbody');
@@ -25,13 +31,28 @@ function reset(){
    tableEl.replaceChild(newTBodyEl, oldTBodyEl);
    document.getElementById("output").style.display = "none";
 }
+function sortByArrival() {
+  for (let i = 0; i < at.length - 1; i++) {
+    for (let j = i + 1; j < at.length; j++) {
+      if (at[i] > at[j]) {
+        [at[i], at[j]] = [at[j], at[i]];
+        [bt[i], bt[j]] = [bt[j], bt[i]];
+        [p[i], p[j]] = [p[j], p[i]];
+        [pr[i], pr[j]] = [pr[j], pr[i]];
+      }
+    }
+  }
+}
+
 function start(){
    let i;
-   rbt = bt;
+   rbt = [...bt];
    // si inseriscono nel corpo della tabella i dati dei processi 
    let tableEl = document.getElementById("idTable");
    let oldTBodyEl = tableEl.getElementsByTagName('tbody')[0];
    let newTBodyEl = document.createElement("tbody");
+  sortByArrival();
+
    for(i=0; i<p.length; i++) {
        const trEl = newTBodyEl.insertRow();
        let tdEl = trEl.insertCell();
@@ -48,7 +69,25 @@ function start(){
    }
    tableEl.replaceChild(newTBodyEl, oldTBodyEl);
    document.getElementById("output").style.display = "block";
+  drawGantt();
+
 }
+function drawGantt() {
+  let gantt = document.querySelector(".progress");
+  gantt.innerHTML = "";
+
+  let total = 0;
+  for (let i = 0; i < bt.length; i++) total += bt[i];
+
+  for (let i = 0; i < p.length; i++) {
+    let bar = document.createElement("div");
+    bar.className = "progress-bar";
+    bar.style.width = (bt[i] / total * 100) + "%";
+    bar.innerText = p[i];
+    gantt.appendChild(bar);
+  }
+}
+
 
 
 
